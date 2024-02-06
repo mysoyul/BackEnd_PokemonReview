b413436e9fbe0436a29869683b37e524558e45a9
mysoyul <vega2k_@naver.com>
2024-02-06 13:19:12 +0900

    SecurityConfig update

--- src/main/java/com/pokemonreview/api/config/security/SecurityConfig.java
+++ src/main/java/com/pokemonreview/api/config/security/SecurityConfig.java
@@ -1,22 +1,31 @@
 package com.pokemonreview.api.config.security;
 
+import com.pokemonreview.api.exceptions.security.CustomAccessDeniedHandler;
+import com.pokemonreview.api.exceptions.security.JwtAuthEntryPoint;
+import com.pokemonreview.api.jwt.filter.JWTAuthenticationFilter;
 import com.pokemonreview.api.service.security.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
     private final CustomUserDetailsService userDetailsService;
 
@@ -36,16 +45,37 @@ public class SecurityConfig {
     }
 
     @Bean
+    public JWTAuthenticationFilter jwtAuthenticationFilter() {
+        return new JWTAuthenticationFilter();
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return new CustomAccessDeniedHandler();
+    }
+
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint() {
+        return new JwtAuthEntryPoint();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 				return http
                         .cors(withDefaults())
                         .csrf(csrf -> csrf.disable())
                         //.csrf(AbstractHttpConfigurer::disable)
-                        .authorizeHttpRequests(auth -> {
-                            auth.requestMatchers("/api/auth/**").permitAll()
+                        .authorizeHttpRequests(auth -> { auth
+                            .requestMatchers("/api/auth/**").permitAll()
                             .requestMatchers("/api/pokemon/**","/api/admin/**").authenticated();
                         })
-                        .formLogin(withDefaults())
+                        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                        .exceptionHandling(authManager -> authManager
+                                .authenticationEntryPoint(authenticationEntryPoint())
+                                .accessDeniedHandler(accessDeniedHandler())
+                        )
+                        //.formLogin(withDefaults())
                         .build();
     }
 }
